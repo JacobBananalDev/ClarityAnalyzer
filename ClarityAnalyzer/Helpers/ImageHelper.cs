706aee01dbe1c1c62f0959b2a7d5bdb9d2bbb305
Jacob Bananal <jacob.bananal@tek84.com>
2025-05-22 10:35:12 -0700

    ClarityAnalyzer - Added ImageHelper.cs

---
+++ ClarityAnalyzer/Helpers/ImageHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ClarityAnalyzer.Helpers
+{
+    internal static class ImageHelper
+    {
+        public static BitmapImage ToBitMapImage(Bitmap source)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                // Save the bitmap to the memory stream.
+                ((System.Drawing.Bitmap)source).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                // Create and initialize the BitmapImage.
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad; // Ensure the stream is fully loaded into memory.
+                ms.Seek(0, SeekOrigin.Begin); // Reset the stream position.
+                image.StreamSource = ms;
+                image.EndInit();
+                return image;
+            }
+        }
+    }
+}
