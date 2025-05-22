0ff3891e391961a0cb6aa328a0644fd4b15fb1cd
Jacob Bananal <jacob.bananal@tek84.com>
2025-05-22 10:06:58 -0700

    ClarityAnalyzer - Added new project for the controls ClarityAnalyzer.Controls. Referenced that project into the main WPF project. Modified the App.xaml to use the custom controls

---
+++ ClarityAnalyzer.Controls/Controls/ClarityAnalyzerButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ClarityAnalyzer.Controls
+{
+    public class ClarityAnalyzerButton : Button
+    {
+        static ClarityAnalyzerButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ClarityAnalyzerButton), new FrameworkPropertyMetadata(typeof(ClarityAnalyzerButton)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register
+            (nameof(CornerRadius), typeof(CornerRadius), typeof(ClarityAnalyzerButton), new PropertyMetadata(default(CornerRadius)));
+    }
+}
