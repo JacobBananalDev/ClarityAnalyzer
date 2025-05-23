1ea4a10e60ab32b6360f0d2480cafcfb1a789b1d
Jacob Bananal <jacob.bananal@tek84.com>
2025-05-23 10:31:19 -0700

    ClarityAnalyzer - Added ClarityAnalzerSlider and ClarityAnalyzerTabControl. Implemented into the MainWindow.xaml

---
+++ ClarityAnalyzer.Controls/Controls/ClarityAnalyzerSlider.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ClarityAnalyzer.Controls
+{
+    public class ClarityAnalyzerSlider : Slider
+    {
+        static ClarityAnalyzerSlider()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ClarityAnalyzerSlider), new FrameworkPropertyMetadata(typeof(ClarityAnalyzerSlider)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register
+            (nameof(CornerRadius), typeof(CornerRadius), typeof(ClarityAnalyzerSlider), new PropertyMetadata(default(CornerRadius)));
+
+        public bool CanIncrement
+        {
+            get { return (bool)GetValue(CanIncrementProperty); }
+            set { SetValue(CanIncrementProperty, value); }
+        }
+        public static readonly DependencyProperty CanIncrementProperty = DependencyProperty.Register
+            (nameof(CanIncrement), typeof(bool), typeof(ClarityAnalyzerSlider), new PropertyMetadata(true));
+
+        public bool CanDecrement
+        {
+            get { return (bool)GetValue(CanDecrementProperty); }
+            set { SetValue(CanDecrementProperty, value); }
+        }
+        public static readonly DependencyProperty CanDecrementProperty = DependencyProperty.Register
+            (nameof(CanDecrement), typeof(bool), typeof(ClarityAnalyzerSlider), new PropertyMetadata(true));
+    }
+}
---
+++ ClarityAnalyzer.Controls/Controls/ClarityAnalyzerTabControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ClarityAnalyzer.Controls
+{
+    public class ClarityAnalyzerTabControl : TabControl
+    {
+        static ClarityAnalyzerTabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ClarityAnalyzerTabControl), new FrameworkPropertyMetadata(typeof(ClarityAnalyzerTabControl)));
+        }
+    }
+}
